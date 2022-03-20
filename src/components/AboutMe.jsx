f8de39bf510ad511f89eae2c838ddf41058e1bdb
unknown <nicolas.farfan.ch@usach.cl>
2022-03-20 13:08:35 -0300

    Correciones del ingles

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,14 +7,15 @@ export const AboutMe = () => {
         <SubTitulo title={"About Me"}></SubTitulo>
         <div className="flex-row flex lg:mt-16 mt-8">
           <h3 className="text-3xl text-justify mt-1 tracking-wide">
-            I am a Software Developer and passionate for the Web Development
-            from Chile, in the last summer I have build some projects to improve
-            my skills in web sites and aplications development, for in the
+            I am a Software Developer and university student, passionate for the
+            Web Development from Chile, in the last summer I have build some
+            projects to improve my skills in web sites development, for in the
             future collaborate in bigger projects and interact with other
-            developers. Now looking for any opportunity to adquire experience in
-            this area, focused in React Apps, with CSS frameworks and web
-            structure. In terms of language, I am not English native speaker,
-            but easily I can express my ideas, writing and talking.
+            developers. So now I am looking for any opportunity to adquire
+            experience in this area, mainly using React, with CSS frameworks,
+            but I also worked with nodeJS, Express and relational databases. In
+            terms of language, I am not English native speaker, but I can easily
+            express my ideas, writing and talking.
           </h3>
         </div>
       </div>
